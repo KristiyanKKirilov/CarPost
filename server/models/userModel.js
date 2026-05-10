0d6cb2201e948be6a475057b19ff834c3177e851
Valentin23123 <petrovvalentin481@gmail.com>
2026-05-10 17:03:50 +0300

    Auth model controller and router

---
+++ server/models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = Number(process.env.SALTROUNDS) || 5;
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    phoneNumber: { type: String, required: true },
+    username: { type: String, required: true, unique: true, minlength: 3 },
+    password: { type: String, required: true, minlength: 5 },
+    cars: [{ type: ObjectId, ref: "Car" }],
+}, { timestamps: true }); // Automatically handles created_at and updatedAt
+
+
+userSchema.methods = {
+    matchPassword: function (password) {
+        return bcrypt.compare(password, this.password);
+    }
+}
+
+userSchema.pre('save', function (next) {
+    if (this.isModified('password')) {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) {
+                next(err);
+            }
+            bcrypt.hash(this.password, salt, (err, hash) => {
+                if (err) {
+                    next(err);
+                }
+                this.password = hash;
+                next();
+            })
+        })
+        return;
+    }
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
